b3ede8b31c850d0c22bab60086ac170d1c54cc0e
Austin McConnell <austin.s.mcconnell@gmail.com>
2018-05-21 06:46:13 -0500

    Initial tox implementation

--- setup.py
+++ setup.py
@@ -11,14 +11,17 @@ setup(
     author_email='austin.s.mcconnell@gmail.com',
     url='https://github.com/austinmcconnell/mortgage',
     packages=find_packages(exclude='tests'),
+    python_requires='>=3.6',
     install_requires=[],
     extras_require={
         'develop': ['bumpversion>=0.5.3,<1.0.0',
+                    'detox>=0.12.0,<1.0.0',
                     'pytest>=3.2.1,<4.0.0',
                     'pytest-cov>=2.5.1,<3.0.0',
                     'recommonmark>=0.4.0,<1.0.0',
                     'sphinx>=1.7.4,<2.0.0',
                     'sphinx-autodoc-typehints>=1.3.0,<2.0.0',
+                    'tox>=3.0.0,<4.0.0',
                     'twine>=1.11.0,<2.0.0',]
     },
     license='MIT license',
@@ -30,6 +33,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests'
 )
